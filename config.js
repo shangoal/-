0d4f463b7d1bcc031077f602a16036b8cb11fe62
shangoal <sandarunethshanhome@gmail.com>
2025-07-14 23:46:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "mNFw1JbA#YoczASuoOob1O4JeL4t3Sr_zpaJLjZtmUvDKWBVS84A",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/shangoal/-/blob/main/images/flex%20Music.jpg?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hey there👋 𝗙𝗹𝗲𝘅 𝗠𝘂𝘀𝗶𝗰 | 🎧🌎 Is Alive Now😍*",
 BOT_OWNER: '94764527598',
